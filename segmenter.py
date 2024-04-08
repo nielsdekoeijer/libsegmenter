aeee75e1176a103fb1442ebb8f76acda350ccdae
mim <mim@bang-olufsen.dk>
2024-04-08 14:00:07 +0200

    Updated torch segmenter

--- segmenter.py
+++ segmenter.py
@@ -1,11 +1,13 @@
 import torch
+import checkCola as cola
 
 class Segmenter(torch.nn.Module):
-    def __init__(self, segment_size, hop_size, window, perform_validation=True, validation_ser=50.0, normalize_window=True):
+    def __init__(self, segment_size, hop_size, window, mode="wola", edge_correction=True, normalize_window=True, device=None, dtype=None):
+        self.factory_kwargs = {"device": device, "dtype": dtype}
         super(Segmenter, self).__init__()
         self.hop_size = hop_size
         self.segment_size = segment_size
-        self.window = window
+        self.window = window.to(device)
 
         # asserts to ensure correctness
         if self.segment_size % 2 != 0:
@@ -19,124 +21,110 @@ class Segmenter(torch.nn.Module):
 
         # compute prewindow and postwindow
         self.prewindow = self.window.clone()
-        i = self.hop_size
-        while i < self.segment_size:
-            self.prewindow[:-i] += self.window[i:]
-            i += self.hop_size
-
         self.postwindow = self.window.clone()
         i = self.hop_size
-        while i < self.segment_size:
-            self.postwindow[i:] += self.window[:-i]
-            i += self.hop_size
+        for h_idx in range(1, self.segment_size // self.hop_size + 1):
+            idx1_start = h_idx * self.hop_size
+            idx1_end = self.segment_size
+            idx2_start = 0
+            idx2_end = self.segment_size - idx1_start
+            self.prewindow[idx2_start:idx2_end] = self.prewindow[idx2_start:idx2_end] + self.window[idx1_start:idx1_end]
+            self.postwindow[idx1_start:idx1_end] = self.postwindow[idx1_start:idx1_end] + self.window[idx2_start:idx2_end]
 
-        # this is a tiny bit hacked, but it works well in practise
+        # Perform normalization of window function
         if normalize_window:
-            normalization = self.prewindow[0]
+            value = cola.checkCola(self.window.numpy(), self.hop_size)
+            normalization = value[1]
             self.window = self.window / normalization
             self.prewindow = self.prewindow / normalization
             self.postwindow = self.postwindow / normalization
+        
+        if edge_correction == False:
+            self.prewindow = self.window
+            self.postwindow = self.window
 
-        # here we ensure that the forward and backward pass have perfrect reconstruction
-        if perform_validation:
-            # here we assert that the window doesn't have a "DC" component
-            # if we are to change the frames individually, such a DC component breaks reconsturction
-            # (imagine problems that exist with e.g. a boxcar window, they will exist for hamming too!)
-            if abs(self.window[0]) > 1e-8 or abs(self.window[-1]) > 1e-8:
-                raise ValueError("filter non-zero at first and / or last entry")
+        if mode == "wola":
+            self.mode = mode
+            self.window = torch.sqrt(self.window)
+            self.prewindow = torch.sqrt(self.prewindow)
+            self.postwindow = torch.sqrt(self.postwindow)
+        elif mode == "ola":
+            self.mode = mode
+        else :
+            raise ValueError(f"only support for model ola and wola")
 
-            # next we assert that the reconstruction forward backward is below some ser in db
-            # not batched
-            itest = torch.randn(segment_size + 10 * hop_size)
-            otest = self.unsegment(self.segment(itest))
-            ser = 20 * torch.log10(torch.norm(otest)) - 20 * torch.log10(torch.norm(itest - otest))
-            if ser < validation_ser:
-                raise ValueError(f"segmentation results in {ser} dB signal to error ratio when max allowable ratio is {validation_ser} dB")
 
-            # batched
-            itest = torch.randn((2, segment_size + 10 * hop_size))
-            otest = self.unsegment(self.segment(itest))
-            ser = 20 * torch.log10(torch.norm(otest)) - 20 * torch.log10(torch.norm(itest - otest))
-            if ser < validation_ser:
-                raise ValueError(f"segmentation results in {ser} dB signal to error ratio when max allowable ratio is {validation_ser} dB")
 
     def segment(self, x):
+        if( (x.dim() == 2) & (x.shape[1] > 1)):
+            number_of_batch_elements = x.shape[0]
+            number_of_samples = x.shape[1]
+        elif (((x.dim() == 2) & (x.shape[1] == 1)) | x.dim() == 1):
+            number_of_batch_elements = 1
+            number_of_samples = x.shape[0]
+        else :
+            raise ValueError(f"only support for inputs with dimension 1 or 2, provided {x.dim()}")
+        
+        number_of_segments = (number_of_samples) // self.hop_size - self.segment_size // self.hop_size + 1
+
         N = x.shape[-1]
         K = (N - self.segment_size) // self.hop_size + 1
 
-        if len(x.shape) == 1:
-            X = torch.zeros((K, self.segment_size))
-            for k in range(K):
-                X[k,:] = x[k * self.hop_size : k * self.hop_size + self.segment_size]
-            return X
-
-        if len(x.shape) == 2:
-            B = x.shape[0]
-            X = torch.zeros((B, K, self.segment_size))
-            for k in range(K):
-                X[:,k,:] = x[:, k * self.hop_size : k * self.hop_size + self.segment_size]
-            return X
-
-        raise ValueError(f"only support for inputs with dimension 1 or 2, provided {len(x.shape)}")
+        if number_of_batch_elements == 1:
+            X = torch.zeros((number_of_segments, self.segment_size), **self.factory_kwargs)
+            if self.mode == "wola":
+                k = 0
+                X[0,:] = self.prewindow * x[k * self.hop_size : k * self.hop_size + self.segment_size]
+                for k in range(1,number_of_frames-1):
+                    X[k,:] = self.window * x[k * self.hop_size : k * self.hop_size + self.segment_size]
+                k = number_of_segments - 1
+                X[k,:] = self.postwindow * x[k * self.hop_size : k * self.hop_size + self.segment_size]
+            else :
+                for k in range(number_of_segments):
+                    X = x[x * self.hop_size : k * self.hop_size + self.segment_size]
+        else :
+            X = torch.zeros((number_of_batch_elements, number_of_segments, self.segment_size), **self.factory_kwargs)
+            for b in range(number_of_batch_elements):
+                if self.mode == "wola":
+                    k = 0
+                    X[:,k,:] = x[:,k * self.hop_size : k * self.hop_size + self.segment_size] * self.prewindow
+                    for k in range(1,number_of_segments - 1):
+                        X[:,k,:] = x[:,k * self.hop_size : k * self.hop_size + self.segment_size] * self.window
+                    k = number_of_segments - 1
+                    X[:,k,:] = x[:,k * self.hop_size : k * self.hop_size + self.segment_size] * self.postwindow
+                else :
+                    for k in range(number_of_segments):
+                        X[:,k,:] = x[:, k * self.hop_size : k * self.hop_size + self.segment_size]
+        return X
 
     def unsegment(self, X):
-        K = X.shape[-2]
-        N = (K - 1) * self.hop_size + self.segment_size
+        if(X.dim() == 3):
+            number_of_batch_elements = X.shape[0]
+            number_of_segments = X.shape[1]
+        elif (X.dim() == 2):
+            number_of_batch_elements = 1
+            number_of_segments = X.shape[0]
+        else :
+            raise ValueError(f"only support for inputs with dimension 2 or 3, provided {X.dim()}")
+        number_of_samples = (number_of_segments - 1) * self.hop_size + self.segment_size
 
-        if len(X.shape) == 2:
-            x = torch.zeros((N))
+
+        if number_of_batch_elements == 1:
+            x = torch.zeros((number_of_samples), **self.factory_kwargs)
             k = 0
             x[k * self.hop_size : k * self.hop_size + self.segment_size] += self.prewindow * X[k]
-            for k in range(1, K - 1):
+            for k in range(1, number_of_segments - 1):
                 x[k * self.hop_size : k * self.hop_size + self.segment_size] += self.window * X[k]
-            k = k + 1
+            k = k + 0
             x[k * self.hop_size : k * self.hop_size + self.segment_size] += self.postwindow * X[k]
-            return x
 
-        if len(X.shape) == 3:
-            B = X.shape[0]
-            x = torch.zeros((B, N))
+        else :
+            x = torch.zeros((number_of_batch_elements, number_of_samples), **self.factory_kwargs)
             k = 0
             x[:, k * self.hop_size : k * self.hop_size + self.segment_size] += self.prewindow * X[:, k, :]
-            for k in range(1, K - 1):
+            for k in range(1, number_of_segments - 1):
                 x[:, k * self.hop_size : k * self.hop_size + self.segment_size] += self.window * X[:, k, :]
             k = k + 1
             x[:, k * self.hop_size : k * self.hop_size + self.segment_size] += self.postwindow * X[:, k, :]
-            return x
+        return x
 
-        raise ValueError(f"only support for inputs with dimension 2 or 3, provided {len(x.shape)}")
-
-if __name__ == "__main__":
-    # quick unit tests
-    import matplotlib
-    matplotlib.use('Agg')
-    import matplotlib.pyplot as plt
-
-    # 1: valid window should create OK
-    try: 
-        window = torch.hann_window(1024, periodic=False)
-        segmenter = Segmenter(1024, 512, window)
-        assert True
-        print(f"passed test 1")
-    except Exception as e:
-        print(e)
-        print(f"failed test 1")
-
-    # 2: more difficult window
-    try: 
-        window = torch.hann_window(3000, periodic=False)
-        segmenter = Segmenter(3000, 1000, window)
-        assert True
-        print(f"passed test 2")
-    except Exception as e:
-        print(e)
-        print(f"failed test 2")
-
-    # 3: messing up periodicity
-    try: 
-        window = torch.hann_window(1024, periodic=True)
-        segmenter = Segmenter(1024, 512, window)
-        assert True
-        print(f"failed test 3")
-    except Exception as e:
-        print(f"passed test 3")
