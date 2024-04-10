d4af523fb54f04d91f4d92f74f61217e28a5550b
Niels de Koeijer <nemk@bang-olufsen.dk>
2024-04-10 14:29:28 +0200

    format

--- format.sh
+++ format.sh
@@ -1,1 +1,4 @@
 find . -name "*.py" -exec black {} +
+find . -name "*.cpp" -exec clang-format -i {} +
+find . -name "*.hpp" -exec clang-format -i {} +
+
--- libsegmenter/SegmenterBindings.cpp
+++ libsegmenter/SegmenterBindings.cpp
@@ -1,51 +1,57 @@
-#include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
 namespace py = pybind11;
 
 #include "Segmenter.hpp"
 using DATATYPE = double;
 
-py::array_t<DATATYPE> py_bartlett(size_t size) {
+py::array_t<DATATYPE> py_bartlett(size_t size)
+{
     auto result = py::array_t<DATATYPE>(size);
     auto buf = result.request();
-    DATATYPE *ptr = static_cast<DATATYPE *>(buf.ptr);
+    DATATYPE* ptr = static_cast<DATATYPE*>(buf.ptr);
     populateBartlettWindow(ptr, size);
     return result;
 }
 
-py::array_t<DATATYPE> py_blackman(size_t size) {
+py::array_t<DATATYPE> py_blackman(size_t size)
+{
     auto result = py::array_t<DATATYPE>(size);
     auto buf = result.request();
-    DATATYPE *ptr = static_cast<DATATYPE *>(buf.ptr);
+    DATATYPE* ptr = static_cast<DATATYPE*>(buf.ptr);
     populateBlackmanWindow(ptr, size);
     return result;
 }
 
-py::array_t<DATATYPE> py_hamming(size_t size) {
+py::array_t<DATATYPE> py_hamming(size_t size)
+{
     auto result = py::array_t<DATATYPE>(size);
     auto buf = result.request();
-    DATATYPE *ptr = static_cast<DATATYPE *>(buf.ptr);
+    DATATYPE* ptr = static_cast<DATATYPE*>(buf.ptr);
     populateHammingWindow(ptr, size);
     return result;
 }
 
-py::array_t<DATATYPE> py_hann(size_t size) {
+py::array_t<DATATYPE> py_hann(size_t size)
+{
     auto result = py::array_t<DATATYPE>(size);
     auto buf = result.request();
-    DATATYPE *ptr = static_cast<DATATYPE *>(buf.ptr);
+    DATATYPE* ptr = static_cast<DATATYPE*>(buf.ptr);
     populateHannWindow(ptr, size);
     return result;
 }
 
-py::array_t<DATATYPE> py_rectangular(size_t size) {
+py::array_t<DATATYPE> py_rectangular(size_t size)
+{
     auto result = py::array_t<DATATYPE>(size);
     auto buf = result.request();
-    DATATYPE *ptr = static_cast<DATATYPE *>(buf.ptr);
+    DATATYPE* ptr = static_cast<DATATYPE*>(buf.ptr);
     populateRectangularWindow(ptr, size);
     return result;
 }
 
-PYBIND11_MODULE(bindings, m) {
+PYBIND11_MODULE(bindings, m)
+{
     m.def("bartlett", py_bartlett);
     m.def("blackman", py_blackman);
     m.def("hamming", py_hamming);
