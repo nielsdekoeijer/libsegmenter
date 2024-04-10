ac05b694cac6f0c267276f4d40c701e5a7431d4d
Niels de Koeijer <nemk@bang-olufsen.dk>
2024-04-10 13:41:32 +0200

    testfix

--- libsegmenter/default_window_selector.py
+++ libsegmenter/default_window_selector.py
@@ -65,102 +65,83 @@ def kaiser(window_length: int, beta: float) -> np.array:
 
 
 def default_window_selector(window_name: str, window_length: int) -> np.array:
-    match window_name:
-        case "bartlett50":
-            # Bartlett (triangular) window with 50% overlap
-            if int(window_length) % 2 != 0:
-                raise ValueError(
-                    "Odd length Bartlett window with 50 percent overlap is not currently supported."
-                )
-            else:
-                window = bartlett(window_length)
-                hopSize = int(window_length) // 2
-                return window, hopSize
-        case "bartlett75":
-            # Bartlett window with 75% overlap
-            if int(window_length) % 4 != 0:
-                raise ValueError(
-                    "Bartlett windows with 75 percent overlap expects a window_length divisible by 4."
-                )
-            else:
-                window = bartlett(window_length)
-                hopSize = int(window_length) // 4
-                return window, hopSize
-        case "blackman":
-            # Blackman window with 2/3 overlap
-            if int(window_length) % 3 != 0:
-                raise ValueError(
-                    "The Blackman window currently only supports overlaps of 2/3."
-                )
-            else:
-                window = blackman(window_length)
-                hopSize = int(window_length) // 3
-                return window, hopSize
-        case "kaiser82":
-            # Kaiser window with beta = 8 and approx 82% overlap
-            beta = 8.0
-            window = kaiser(window_length, beta)
-            hopSize = int(
-                np.floor(1.7 * (np.float64(window_length) - 1.0) / (beta + 1.0))
-            )
-            return window, hopSize
-        case "kaiser85":
-            # Kaiser window with beta = 10 and approx 85% overlap
-            beta = 10.0
-            window = kaiser(window_length, beta)
-            hopSize = int(
-                np.floor(1.7 * (np.float64(window_length) - 1.0) / (beta + 1.0))
-            )
-            return window, hopSize
-        case "hamming50":
-            # Hamming window with 50% overlap
-            if window_length % 2 != 0:
-                raise ValueError(
-                    "Odd length Hamming window at 50 percent overlap is not currently supported."
-                )
-            else:
-                window = hamming(window_length)
-                hopSize = int(window_length) // 2
-                return window, hopSize
-        case "hamming75":
-            # Hamming window with 75% overlap
-            if int(window_length) % 4 != 0:
-                raise ValueError(
-                    "For Hamming windows with 75 percent overlay, the window_length is expected to be divisible by 4."
-                )
-            else:
-                window = hamming(window_length)
-                hopSize = int(window_length) // 4
-                return window, hopSize
-        case "hann50":
-            # Hann window with 50% overlap
-            if int(window_length) % 2 != 0:
-                raise ValueError(
-                    "Odd length Hann window at 50 percent overlap is not currently supported."
-                )
-            else:
-                window = hann(window_length)
-                hopSize = int(window_length) // 2
-                return window, hopSize
-        case "hann75":
-            # Hann window with 75% overlap
-            if int(window_length) % 4 != 0:
-                raise ValueError(
-                    "For Hann windows with 75 percent overlap, the window_length is expected to be divisible by 4."
-                )
-            else:
-                window = hann(window_length)
-                hopSize = int(window_length) // 4
-                return window, hopSize
-        case "rectangular0":
-            # Rectangular window with 0% overlap
-            window = np.ones(window_length, dtype=np.float64)
-            hopSize = int(window_length)
-            return window, hopSize
-        case "rectangular50":
-            # Rectangular window with 50% overlap
-            window = np.ones(window_length, dtype=np.float64)
+    if window_name == "bartlett50":
+        # Bartlett (triangular) window with 50% overlap
+        if int(window_length) % 2 != 0:
+            raise ValueError("Odd length Bartlett window with 50 percent overlap is not currently supported.")
+        else:
+            window = bartlett(window_length)
             hopSize = int(window_length) // 2
             return window, hopSize
-        case _:
-            raise ValueError("No valid window_name was provided.")
+    elif window_name == "bartlett75":
+        # Bartlett window with 75% overlap
+        if int(window_length) % 4 != 0:
+            raise ValueError("Bartlett windows with 75 percent overlap expects a window_length divisible by 4.")
+        else:
+            window = bartlett(window_length)
+            hopSize = int(window_length) // 4
+            return window, hopSize
+    elif window_name == "blackman":
+        # Blackman window with 2/3 overlap
+        if int(window_length) % 3 != 0:
+            raise ValueError("The Blackman window currently only supports overlaps of 2/3.")
+        else:
+            window = blackman(window_length)
+            hopSize = int(window_length) // 3
+            return window, hopSize
+    elif window_name == "kaiser82":
+        # Kaiser window with beta = 8 and approx 82% overlap
+        beta = 8.0
+        window = kaiser(window_length, beta)
+        hopSize = int(np.floor(1.7 * (np.float64(window_length) - 1.0) / (beta + 1.0)))
+        return window, hopSize
+    elif window_name == "kaiser85":
+        # Kaiser window with beta = 10 and approx 85% overlap
+        beta = 10.0
+        window = kaiser(window_length, beta)
+        hopSize = int(np.floor(1.7 * (np.float64(window_length) - 1.0) / (beta + 1.0)))
+        return window, hopSize
+    elif window_name == "hamming50":
+        # Hamming window with 50% overlap
+        if window_length % 2 != 0:
+            raise ValueError("Odd length Hamming window at 50 percent overlap is not currently supported.")
+        else:
+            window = hamming(window_length)
+            hopSize = int(window_length) // 2
+            return window, hopSize
+    elif window_name == "hamming75":
+        # Hamming window with 75% overlap
+        if int(window_length) % 4 != 0:
+            raise ValueError("For Hamming windows with 75 percent overlay, the window_length is expected to be divisible by 4.")
+        else:
+            window = hamming(window_length)
+            hopSize = int(window_length) // 4
+            return window, hopSize
+    elif window_name == "hann50":
+        # Hann window with 50% overlap
+        if int(window_length) % 2 != 0:
+            raise ValueError("Odd length Hann window at 50 percent overlap is not currently supported.")
+        else:
+            window = hann(window_length)
+            hopSize = int(window_length) // 2
+            return window, hopSize
+    elif window_name == "hann75":
+        # Hann window with 75% overlap
+        if int(window_length) % 4 != 0:
+            raise ValueError("For Hann windows with 75 percent overlap, the window_length is expected to be divisible by 4.")
+        else:
+            window = hann(window_length)
+            hopSize = int(window_length) // 4
+            return window, hopSize
+    elif window_name == "rectangular0":
+        # Rectangular window with 0% overlap
+        window = np.ones(window_length, dtype=np.float64)
+        hopSize = int(window_length)
+        return window, hopSize
+    elif window_name == "rectangular50":
+        # Rectangular window with 50% overlap
+        window = np.ones(window_length, dtype=np.float64)
+        hopSize = int(window_length) // 2
+        return window, hopSize
+    else:
+        raise ValueError("No valid window_name was provided.")
