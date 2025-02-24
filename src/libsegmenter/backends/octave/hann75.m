be6ef1b6400fb799a57464f388b813aed39e4b10
macoustics <mim@bang-olufsen.dk>
2025-02-24 10:55:58 +0100

    Update matlab implementation to new structure

---
+++ src/libsegmenter/backends/octave/hann75.m
@@ -0,0 +1,18 @@
+function [window, hopSize] = hann75(segmentSize)
+    % Generates a Hann window of the given size with 75% overlap.
+    %
+    % Args:
+    %   segmentSize (double): Size of the window to be created.
+    %
+    % Returns:
+    %   A Hann window with 75% overlap
+    if length(segmentSize) ~= 1
+        error(['The segment size must be a scalar. Received size(segmentSize) = ' int2str(size(segmentSize))] );
+    end
+    if mod(segmentSize,4)
+        error(['Segment size must be integer divisible by 4, got ' int2str(segmentSize) ]);
+    end
+    indices = (0:segmentSize-1)';
+    window = _hann(segmentSize, indices);
+    hopSize = segmentSize/4;
+end
